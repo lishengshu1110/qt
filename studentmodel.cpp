547261752036fd7c9b65c5c9bc20705df27907d2
lishengshu1110 <2065660032@qq.com>
2026-01-15 01:23:52 +0800

    对学生数据模型进行初始化并实现其行列数的基础接口

--- studentmodel.cpp
+++ studentmodel.cpp
@@ -1,3 +1,19 @@
-#include "studentmodel.h"
+#include "StudentModel.h"
 
-StudentModel::StudentModel(){}
+StudentModel::StudentModel(QObject* parent)
+    : QAbstractTableModel(parent)
+{
+    m_headers << "学号" << "姓名" << "班级";
+}
+
+int StudentModel::rowCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return m_students.size();
+}
+
+int StudentModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return m_headers.size();
+}
