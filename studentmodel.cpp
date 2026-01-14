08ec17db2bcb00b26d29ed481f9fbd940917f2ac
lishengshu1110 <2065660032@qq.com>
2026-01-15 01:26:53 +0800

    实现了学生数据的展示功能。

--- studentmodel.cpp
+++ studentmodel.cpp
@@ -17,3 +17,24 @@ int StudentModel::columnCount(const QModelIndex& parent) const
     Q_UNUSED(parent);
     return m_headers.size();
 }
+
+QVariant StudentModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() || index.row() >= m_students.size()) {
+        return QVariant();
+    }
+
+    if (role == Qt::DisplayRole || role == Qt::EditRole) {
+        const QStringList& student = m_students.at(index.row());
+        if (index.column() < student.size()) {
+            return student.at(index.column());
+        }
+    }
+
+    if (role == Qt::TextAlignmentRole) {
+        return Qt::AlignCenter;
+    }
+
+    return QVariant();
+}
+
