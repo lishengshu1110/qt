069f7d4c723af5a6abb758a442039198896ca837
lishengshu1110 <2065660032@qq.com>
2026-01-14 23:55:37 +0800

    初始化模型并实现了行列数的基础接口

--- grademodel.cpp
+++ grademodel.cpp
@@ -1,3 +1,20 @@
-#include "grademodel.h"
+#include "GradeModel.h"
+#include <QColor>
 
-GradeModel::GradeModel() {}
+GradeModel::GradeModel(QObject* parent)
+    : QAbstractTableModel(parent)
+{
+    m_headers << "学号" << "姓名" << "课程编号" << "课程名称" << "成绩" << "学期";
+}
+
+int GradeModel::rowCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return m_grades.size();
+}
+
+int GradeModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return m_headers.size();
+}
