c0a7fd080219478277bc2927916e78d3a35ceea0
lishengshu1110 <2065660032@qq.com>
2026-01-15 11:40:33 +0800

    初始化了统计图表的组件基础结构和ui布局，添加图表类型的下拉选择框等。

--- statisticswidget.cpp
+++ statisticswidget.cpp
@@ -1,3 +1,41 @@
-#include "statisticswidget.h"
+#include "StatisticsWidget.h"
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QValueAxis>
+#include <QtCharts/QLineSeries>
+#include <QPainter>
+#include <QDebug>
 
-StatisticsWidget::StatisticsWidget() {}
+StatisticsWidget::StatisticsWidget(QWidget* parent)
+    : QWidget(parent)
+    , m_chart(nullptr)
+{
+    setupUI();
+}
+
+void StatisticsWidget::setupUI()
+{
+    m_mainLayout = new QVBoxLayout(this);
+
+    // 控制栏
+    m_controlLayout = new QHBoxLayout();
+    m_controlLayout->addWidget(new QLabel("图表类型:"));
+
+    m_chartTypeCombo = new QComboBox(this);
+    m_chartTypeCombo->addItem("柱状图");
+    m_chartTypeCombo->addItem("折线图");
+    connect(m_chartTypeCombo, QOverload<int>::of(&QComboBox::currentIndexChanged),
+            this, &StatisticsWidget::onChartTypeChanged);
+
+    m_controlLayout->addWidget(m_chartTypeCombo);
+    m_controlLayout->addStretch();
+
+    m_mainLayout->addLayout(m_controlLayout);
+
+    // 图表视图
+    m_chartView = new QChartView(this);
+    m_chartView->setRenderHint(QPainter::Antialiasing);
+    m_mainLayout->addWidget(m_chartView);
+}
+
