e6d2639cddc7d5568b8b2a015d8823578c88183f
lishengshu1110 <2065660032@qq.com>
2026-01-15 12:19:45 +0800

    初始化了主窗口的核心结构与全局配置，以及实现四大功能标签页UI布局与控件绑定：学生，课程，成绩管理页：表格+表单操作区，绑定增删改查按钮信号统计分析页：初始化StatisticsWidget并添加到TabWidget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,20 +1,235 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "MainWindow.h"
+#include <QMenuBar>
+#include <QMenu>
+#include <QToolBar>
+#include <QHeaderView>
+#include <QFormLayout>
+#include <QGridLayout>
+#include <QStandardItemModel>
+#include <QStandardItem>
+#include <QApplication>
+#include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , m_studentModel(nullptr)
+    , m_gradeModel(nullptr)
 {
-    ui->setupUi(this);
+    // 初始化数据库
+    m_dbManager = &DatabaseManager::getInstance();
+    if (!m_dbManager->initializeDatabase()) {
+        QMessageBox::critical(this, "错误", "数据库初始化失败！");
+    }
+
+    // 初始化文件管理
+    m_fileManager = new FileManager(this);
+
+    setupUI();
+    setupMenuBar();
+    setupStatusBar();
+    connectSignals();
+    refreshAllData();
+
+    setWindowTitle("学生成绩管理系统");
+    resize(1200, 800);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
 }
 
-void MainWindow::on_actionSyncData_triggered()
+void MainWindow::setupUI()
 {
+    m_tabWidget = new QTabWidget(this);
+    setCentralWidget(m_tabWidget);
 
+    setupStudentTab();
+    setupCourseTab();
+    setupGradeTab();
+    setupStatisticsTab();
+}
+
+void MainWindow::setupStudentTab()
+{
+    m_studentTab = new QWidget();
+    QVBoxLayout* mainLayout = new QVBoxLayout(m_studentTab);
+
+    // 表格视图
+    m_studentTable = new QTableView();
+    m_studentModel = new StudentModel(this);
+    m_studentTable->setModel(m_studentModel);
+    m_studentTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    m_studentTable->setSelectionMode(QAbstractItemView::SingleSelection);
+    m_studentTable->horizontalHeader()->setStretchLastSection(true);
+    m_studentTable->setAlternatingRowColors(true);
+
+    mainLayout->addWidget(m_studentTable);
+
+    // 操作区域
+    QGroupBox* operationGroup = new QGroupBox("学生信息操作");
+    QFormLayout* formLayout = new QFormLayout(operationGroup);
+
+    m_studentIdEdit = new QLineEdit();
+    m_studentNameEdit = new QLineEdit();
+    m_classNameEdit = new QLineEdit();
+
+    formLayout->addRow("学号:", m_studentIdEdit);
+    formLayout->addRow("姓名:", m_studentNameEdit);
+    formLayout->addRow("班级:", m_classNameEdit);
+
+    QHBoxLayout* buttonLayout = new QHBoxLayout();
+    QPushButton* addBtn = new QPushButton("添加");
+    QPushButton* updateBtn = new QPushButton("更新");
+    QPushButton* deleteBtn = new QPushButton("删除");
+    QPushButton* refreshBtn = new QPushButton("刷新");
+
+    connect(addBtn, &QPushButton::clicked, this, &MainWindow::onAddStudent);
+    connect(updateBtn, &QPushButton::clicked, this, &MainWindow::onUpdateStudent);
+    connect(deleteBtn, &QPushButton::clicked, this, &MainWindow::onDeleteStudent);
+    connect(refreshBtn, &QPushButton::clicked, this, &MainWindow::onRefreshStudents);
+
+    buttonLayout->addWidget(addBtn);
+    buttonLayout->addWidget(updateBtn);
+    buttonLayout->addWidget(deleteBtn);
+    buttonLayout->addWidget(refreshBtn);
+    buttonLayout->addStretch();
+
+    formLayout->addRow(buttonLayout);
+    mainLayout->addWidget(operationGroup);
+
+    m_tabWidget->addTab(m_studentTab, "学生管理");
+}
+
+void MainWindow::setupCourseTab()
+{
+    m_courseTab = new QWidget();
+    QVBoxLayout* mainLayout = new QVBoxLayout(m_courseTab);
+
+    // 表格视图（使用标准模型）
+    m_courseTable = new QTableView();
+    QStandardItemModel* courseModel = new QStandardItemModel(this);
+    courseModel->setHorizontalHeaderLabels(QStringList() << "课程编号" << "课程名称" << "学分");
+    m_courseTable->setModel(courseModel);
+    m_courseTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    m_courseTable->horizontalHeader()->setStretchLastSection(true);
+    m_courseTable->setAlternatingRowColors(true);
+
+    mainLayout->addWidget(m_courseTable);
+
+    // 操作区域
+    QGroupBox* operationGroup = new QGroupBox("课程信息操作");
+    QFormLayout* formLayout = new QFormLayout(operationGroup);
+
+    m_courseIdEdit = new QLineEdit();
+    m_courseNameEdit = new QLineEdit();
+    m_creditsEdit = new QLineEdit();
+
+    formLayout->addRow("课程编号:", m_courseIdEdit);
+    formLayout->addRow("课程名称:", m_courseNameEdit);
+    formLayout->addRow("学分:", m_creditsEdit);
+
+    QHBoxLayout* buttonLayout = new QHBoxLayout();
+    QPushButton* addBtn = new QPushButton("添加");
+    QPushButton* updateBtn = new QPushButton("更新");
+    QPushButton* deleteBtn = new QPushButton("删除");
+    QPushButton* refreshBtn = new QPushButton("刷新");
+
+    connect(addBtn, &QPushButton::clicked, this, &MainWindow::onAddCourse);
+    connect(updateBtn, &QPushButton::clicked, this, &MainWindow::onUpdateCourse);
+    connect(deleteBtn, &QPushButton::clicked, this, &MainWindow::onDeleteCourse);
+    connect(refreshBtn, &QPushButton::clicked, this, &MainWindow::onRefreshCourses);
+
+    buttonLayout->addWidget(addBtn);
+    buttonLayout->addWidget(updateBtn);
+    buttonLayout->addWidget(deleteBtn);
+    buttonLayout->addWidget(refreshBtn);
+    buttonLayout->addStretch();
+
+    formLayout->addRow(buttonLayout);
+    mainLayout->addWidget(operationGroup);
+
+    m_tabWidget->addTab(m_courseTab, "课程管理");
+}
+
+void MainWindow::setupGradeTab()
+{
+    m_gradeTab = new QWidget();
+    QVBoxLayout* mainLayout = new QVBoxLayout(m_gradeTab);
+
+    // 筛选区域
+    QGroupBox* filterGroup = new QGroupBox("筛选条件");
+    QHBoxLayout* filterLayout = new QHBoxLayout(filterGroup);
+
+    filterLayout->addWidget(new QLabel("学生:"));
+    m_filterStudentCombo = new QComboBox();
+    m_filterStudentCombo->addItem("全部");
+    m_filterStudentCombo->setEditable(false);
+
+    filterLayout->addWidget(new QLabel("课程:"));
+    m_filterCourseCombo = new QComboBox();
+    m_filterCourseCombo->addItem("全部");
+    m_filterCourseCombo->setEditable(false);
+
+    QPushButton* filterBtn = new QPushButton("筛选");
+    connect(filterBtn, &QPushButton::clicked, this, &MainWindow::onFilterGrades);
+
+    filterLayout->addWidget(m_filterStudentCombo);
+    filterLayout->addWidget(m_filterCourseCombo);
+    filterLayout->addWidget(filterBtn);
+    filterLayout->addStretch();
+
+    mainLayout->addWidget(filterGroup);
+
+    // 表格视图
+    m_gradeTable = new QTableView();
+    m_gradeModel = new GradeModel(this);
+    m_gradeTable->setModel(m_gradeModel);
+    m_gradeTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    m_gradeTable->horizontalHeader()->setStretchLastSection(true);
+    m_gradeTable->setAlternatingRowColors(true);
+
+    mainLayout->addWidget(m_gradeTable);
+
+    // 操作区域
+    QGroupBox* operationGroup = new QGroupBox("成绩信息操作");
+    QFormLayout* formLayout = new QFormLayout(operationGroup);
+
+    m_gradeStudentCombo = new QComboBox();
+    m_gradeCourseCombo = new QComboBox();
+    m_scoreEdit = new QLineEdit();
+    m_semesterEdit = new QLineEdit();
+
+    formLayout->addRow("学生:", m_gradeStudentCombo);
+    formLayout->addRow("课程:", m_gradeCourseCombo);
+    formLayout->addRow("成绩:", m_scoreEdit);
+    formLayout->addRow("学期:", m_semesterEdit);
+
+    QHBoxLayout* buttonLayout = new QHBoxLayout();
+    QPushButton* addBtn = new QPushButton("添加");
+    QPushButton* updateBtn = new QPushButton("更新");
+    QPushButton* deleteBtn = new QPushButton("删除");
+    QPushButton* refreshBtn = new QPushButton("刷新");
+
+    connect(addBtn, &QPushButton::clicked, this, &MainWindow::onAddGrade);
+    connect(updateBtn, &QPushButton::clicked, this, &MainWindow::onUpdateGrade);
+    connect(deleteBtn, &QPushButton::clicked, this, &MainWindow::onDeleteGrade);
+    connect(refreshBtn, &QPushButton::clicked, this, &MainWindow::onRefreshGrades);
+
+    buttonLayout->addWidget(addBtn);
+    buttonLayout->addWidget(updateBtn);
+    buttonLayout->addWidget(deleteBtn);
+    buttonLayout->addWidget(refreshBtn);
+    buttonLayout->addStretch();
+
+    formLayout->addRow(buttonLayout);
+    mainLayout->addWidget(operationGroup);
+
+    m_tabWidget->addTab(m_gradeTab, "成绩管理");
+}
+
+void MainWindow::setupStatisticsTab()
+{
+    m_statisticsWidget = new StatisticsWidget();
+    m_tabWidget->addTab(m_statisticsWidget, "统计分析");
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,22 +2,119 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTabWidget>
+#include <QTableView>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+#include <QComboBox>
+#include <QProgressBar>
+#include <QStatusBar>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGroupBox>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QSplitter>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
+#include "DatabaseManager.h"
+#include "StudentModel.h"
+#include "GradeModel.h"
+#include "FileManager.h"
+#include "StatisticsWidget.h"
 
+/**
+ * @brief 主窗口类
+ * 学生成绩管理系统的主界面
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private slots:
+    // 学生管理
+    void onAddStudent();
+    void onUpdateStudent();
+    void onDeleteStudent();
+    void onRefreshStudents();
+
+    // 课程管理
+    void onAddCourse();
+    void onUpdateCourse();
+    void onDeleteCourse();
+    void onRefreshCourses();
+
+    // 成绩管理
+    void onAddGrade();
+    void onUpdateGrade();
+    void onDeleteGrade();
+    void onRefreshGrades();
+    void onFilterGrades();
+
+    // 文件操作
+    void onImportStudents();
+    void onImportCourses();
+    void onImportGrades();
+    void onExportData();
+
+    // 统计与同步
+    void onUpdateStatistics();
+    void onSyncData();
+
 private:
-    Ui::MainWindow *ui;
+    void setupUI();
+    void setupStudentTab();
+    void setupCourseTab();
+    void setupGradeTab();
+    void setupStatisticsTab();
+    void setupMenuBar();
+    void setupStatusBar();
+    void connectSignals();
+    void refreshAllData();
+
+    // UI组件
+    QTabWidget* m_tabWidget;
+
+    // 学生管理标签页
+    QWidget* m_studentTab;
+    QTableView* m_studentTable;
+    StudentModel* m_studentModel;
+    QLineEdit* m_studentIdEdit;
+    QLineEdit* m_studentNameEdit;
+    QLineEdit* m_classNameEdit;
+
+    // 课程管理标签页
+    QWidget* m_courseTab;
+    QTableView* m_courseTable;
+    QLineEdit* m_courseIdEdit;
+    QLineEdit* m_courseNameEdit;
+    QLineEdit* m_creditsEdit;
+
+    // 成绩管理标签页
+    QWidget* m_gradeTab;
+    QTableView* m_gradeTable;
+    GradeModel* m_gradeModel;
+    QComboBox* m_gradeStudentCombo;
+    QComboBox* m_gradeCourseCombo;
+    QLineEdit* m_scoreEdit;
+    QLineEdit* m_semesterEdit;
+    QComboBox* m_filterStudentCombo;
+    QComboBox* m_filterCourseCombo;
+
+    // 统计标签页
+    StatisticsWidget* m_statisticsWidget;
+
+    // 工具栏
+    QProgressBar* m_progressBar;
+    QLabel* m_statusLabel;
+
+    // 数据管理
+    DatabaseManager* m_dbManager;
+    FileManager* m_fileManager;
 };
+
 #endif // MAINWINDOW_H
