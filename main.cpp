d660397731d0ffb98cf3ccd76c50f00bc9a70334
lishengshu1110 <2065660032@qq.com>
2026-01-15 13:59:06 +0800

    在main.cpp中设置了应用信息并创建了主窗口。

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
-#include "mainwindow.h"
-
+#include "MainWindow.h"
 #include <QApplication>
+#include <QStyleFactory>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    // 设置应用程序信息
+    app.setApplicationName("学生成绩管理系统");
+    app.setApplicationVersion("1.0");
+    app.setOrganizationName("Qt课程设计");
+
+    // 设置样式
+    app.setStyle(QStyleFactory::create("Fusion"));
+
+    // 创建主窗口
+    MainWindow window;
+    window.show();
+
+    return app.exec();
 }
